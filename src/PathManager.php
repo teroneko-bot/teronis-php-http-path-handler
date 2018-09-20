c88cf62246621db4d5298d13e38dd35bfafb4ae6
C. Loncar <giteroneko@gmail.com>
2018-09-20 10:05:05 +0200

    you can now pass a callback for handlePathesWithErrorHandling() to return a customized object that represents a exception;

--- src/PathManager.php
+++ src/PathManager.php
@@ -2,8 +2,10 @@
 
 use MabeEnum\EnumSet;
 
-class PathManager {
-    private static function getHttpPostKeyring(): ?IHttpMethodKeyring {
+class PathManager
+{
+    private static function getHttpPostKeyring(): ?IHttpMethodKeyring
+    {
         if (array_key_exists("CONTENT_TYPE", $_SERVER)) {
             $contentPart = $_SERVER["CONTENT_TYPE"];
 
@@ -14,14 +16,15 @@ class PathManager {
             }
         }
 
-        return NULL;
+        return null;
     }
 
-    private static function getDefaultSharePoint(PathManager $pathManager): \stdClass{
+    private static function getDefaultSharePoint(PathManager $pathManager): \stdClass
+    {
         // default share point
         $sharePoint = new \stdClass();
         $sharePoint->skipTokenValidation = false;
-        $sharePoint->tokenValidationSkipReason = NULL;
+        $sharePoint->tokenValidationSkipReason = null;
         $sharePoint->nextHandlePathesRun = false;
         // keyring
         $spKeyring = new SharePointKeyring();
@@ -36,7 +39,8 @@ class PathManager {
     private $pathes;
     private $httpMethodKeyrings;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->pathes = array();
 
         $this->httpMethodKeyrings = [
@@ -53,11 +57,13 @@ class PathManager {
     }
 
     // key value pairs
-    public function registerPath(PathBase $path) {
+    public function registerPath(PathBase $path)
+    {
         array_push($this->pathes, $path);
     }
 
-    public function handlePathesWithErrorHandling($callback) {
+    public function handlePathesWithErrorHandling($fullStackCallback, ?callable $singleErrorCallback = null)
+    {
         try {
             $this->handlePathes();
         } catch (\Exception $error) {
@@ -65,24 +71,23 @@ class PathManager {
             $lastError = $error;
 
             do {
-                $lastErrorObj = getErrorObject($lastError);
-
-                if ($lastError instanceof \PathException) {
-                    attachPathExceptionInfos($lastErrorObj, $lastError);
-                }
-
-                if ($lastError instanceof \PDOException) {
-                    attachPDOExceptionInfos($lastErrorObj, $lastError);
+                if (is_callable($singleErrorCallback)) {
+                    $lastErrorObj = $singleErrorCallback($lastError);
+                } else {
+                    $lastErrorObj = new \stdClass;
+                    $lastErrorObj->type = get_class($lastError);
+                    $lastErrorObj->message = $lastError->getMessage();
                 }
 
                 array_push($exceptions, $lastErrorObj);
             } while (!is_null($lastError = $lastError->getPrevious()));
 
-            $callback($exceptions);
+            $fullStackCallback($exceptions);
         }
     }
 
-    public function handlePathes(?\stdClass $sharePoint = NULL) {
+    public function handlePathes(?\stdClass $sharePoint = null)
+    {
         if (!is_null($sharePoint)) {
             // increase by one
             $sharePoint->handlePathesRun++;
@@ -114,7 +119,8 @@ class PathManager {
         }
     }
 
-    public function hasAnyHttpMethodKeyring(EnumSet $paramTypeSet, ?array &$paramTypeValueIntersection = NULL): bool {
+    public function hasAnyHttpMethodKeyring(EnumSet $paramTypeSet, ?array &$paramTypeValueIntersection = null): bool
+    {
         $first = $paramTypeSet->getValues();
         $keyrings = $this->httpMethodKeyrings;
         $second = array_keys($keyrings);
@@ -123,7 +129,8 @@ class PathManager {
         return $isNotEmpty;
     }
 
-    public function hasAnyHttpKey(Parameter $param, &$firstKeyring = NULL, ?array $paramTypeValueIntersection = NULL) {
+    public function hasAnyHttpKey(Parameter $param, &$firstKeyring = null, ?array $paramTypeValueIntersection = null)
+    {
         $paramTypeSet = $param->getParamTypeSet();
         $hasAnyKeyring = false;
 
